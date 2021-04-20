aba5544a9d57c4034b239e357c0d8811b738bd7d
Pedro Arthur <slzpedroarthurcp@protonmail.com>
2021-04-20 07:19:10 -0300

    mae

--- ML/metrics/regression.py
+++ ML/metrics/regression.py
@@ -9,15 +9,16 @@ def accuracy(actual, predicted):
 			correct += 1
 	return correct / float(len(actual)) * 100.0
 
+# Média quadrada do erro
 def mse(actual, predicted):
    sum_error = 0.0
-	for i in range(len(actual)):
-		error_i = predicted[i] - actual[i]
-		sum_error += (error_i ** 2)
-	mean_error = sum_error / float(len(actual))
-	return math.sqrt(mean_error)
+   for i in range(len(actual)):
+      error_i = predicted[i] - actual[i]
+      sum_error += (error_i ** 2)
+   mean_error = sum_error / float(len(actual))
+   return math.sqrt(mean_error)
 
-def rmse(actual, predicted):
+def rmse_simple(actual, predicted):
    return math.sqrt(mse(actual, predicted))
 
 # Raiz quadrada da média de erro
@@ -29,9 +30,27 @@ def rmse(actual, predicted):
 	mean_error = square_sum / float(len(actual))
 	return math.sqrt(mean_error)
 
+# Média do valor obsoluto
+def mae(actual, predicted):
+   sum_error = 0.0
+   for i in range(len(actual)):
+      sum_error += abs(predicted[i] - actual[i])
+   return sum_error / float(len(actual))
 
-estimation = [2.3, 1.5]
 validation_data = [2, 1]
+estimation = [2.3, 1.5]
 
-print("RSME = {0:.2f} \nAccuracy = {1:.2f}".format(rmse(validation_data, estimation),accuracy(validation_data, estimation)))
+print("\nAccuracy = {1:.3f} \nMSE = {0:.3f}".format(
+   accuracy(validation_data, estimation),
+   mse(validation_data, estimation)))
 
+validation_data = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+estimation = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0]
+
+print("\MSE = {1:.3f} \nRMSE = {0:.3f}".format(
+   mse(validation_data, estimation),
+   rmse(validation_data, estimation),
+   mae(validation_data, estimation)))
+
+mae = mae(estimation, predicted)
+print(mae)
